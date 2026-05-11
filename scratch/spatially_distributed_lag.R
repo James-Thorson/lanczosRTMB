fe3ce7016abbf80ccf0fde7cf330dabbab03d1e3
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2026-05-11 09:19:17 -0700

    Create spatially_distributed_lag.R

---
+++ scratch/spatially_distributed_lag.R
@@ -0,0 +1,73 @@
+
+library(Matrix)
+library(lanczosRTMB)
+
+###############
+# Lanczos for covariate diffusion
+#  SPDE:  Dinv = I + kappa^-2 C^-1 G
+#  AR:  Dinv = I - P
+###############
+
+n = 10001
+kappa = exp(- 2 / n )
+P = bandSparse( n = n, k = c(-1,1), diag = list(rep(0.5,n),rep(0.5,n)) )
+I = V = Diagonal(n = n)
+#Q = (I - kappa * t(P)) %*% solve(V) %*% (I - kappa * P)
+x = rep(0, n)
+x[ceiling(n/2)] = 1
+
+if( n <= 1001 ){
+  D0 = expm( kappa*P - Diagonal(n=n) )
+}else{
+  D0 = sparseMatrix( i = 1, j = 1, x = 0, dims = c(n,n) )
+}
+
+invD = I - kappa * P
+D = solve(invD)
+
+Hq = \(q,x) (invD %*% q)[,1]
+
+# Lanczos
+L = lanczos(Hq, q = x, k = ceiling(n / 10), x = rep(0,n) )
+T = tridiag( L$alpha, L$beta )
+D1 = L$Q %*% solve(T) %*% t(L$Q)
+
+# CG
+cg_solve = CG( x, Hq, x = x, e = 1e-4 )
+
+Y = cbind(D0 %*% x, D %*% x, D1 %*% x, cg_solve$x )
+matplot(
+  scale(Y, scale = colSums(Y), center = FALSE ),
+  type = "l",
+  lwd = 2,
+  lty = "solid",
+  col = rainbow(4)
+)
+legend( "topright", bty = "n", fill = rainbow(4),
+        legend = c("expm","implicit","lanczos","CG") )
+
+
+
+###############
+# Initial exploration
+###############
+
+#n = 100
+#kappa = exp(-10 / n )
+#P = bandSparse( n = n, k = c(-1,1), diag = list(rep(0.5,n),rep(0.5,n)) )
+#I = V = Diagonal(n = n)
+#Q = (I - kappa * t(P)) %*% solve(V) %*% (I - kappa * P)
+#
+#invD = I - kappa * P
+#D = solve(invD)
+#
+#Hq = \(q,x) (invD %*% q)[,1]
+#q = sample( c(-1,1), size = n, replace = TRUE )
+#L = lanczos(Hq, q = q, k = 30, x = rep(0,n) )
+#T = tridiag( L$alpha, L$beta )
+#
+#D1 = L$Q %*% solve(T) %*% t(L$Q)
+#
+#cols = ceiling(seq(1,n,length = 6))
+#matplot(D1[,cols], type = "l", col = viridisLite::viridis(n), lty = "solid" )
+
