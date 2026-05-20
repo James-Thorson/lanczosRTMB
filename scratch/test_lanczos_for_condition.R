afe2b82918e9a4ac3fdf3d1922359a6a256e7e4c
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2026-05-20 11:09:40 -0700

    Create test_lanczos_for_condition.R

---
+++ scratch/test_lanczos_for_condition.R
@@ -0,0 +1,27 @@
+
+library(lanczosRTMB)
+library(Matrix)
+
+# Settings
+n = 10^3
+rho = 0.99
+
+# Simulate AR1 process approaching random walk (i.e., ill-conditioned inner problem)
+P = bandSparse( n = n, k = c(-1), diagonals = list(rep(1,n)) )
+Q = (Diagonal(n) - rho * t(P)) %*% (Diagonal(n) - rho * P)
+eigval = eigen(Q)$values
+
+#
+Hq = function(q,x) as.vector(Q%*%q)
+L = lanczos(
+  Hq,
+  q = sample( c(-1,1), size = n, replace = TRUE ),
+  k = 30,
+  x = rep(0, n)
+)
+T = tridiag( L$alpha, L$beta )
+eigval2 = eigen(T)$values
+
+mean(eigval^4)
+mean(eigval2^4)
+
